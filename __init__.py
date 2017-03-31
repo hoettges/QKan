58dd0025070c7644fea7b9abfc7d5111f925589c
Jörg Höttges <hoettges@fh-aachen.de>
2017-03-31 14:36:15 +0200

    Korrektur 31.3.2017

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 """
 /***************************************************************************
- Gangliniengrafik
+ Laengsschnitt
                                  A QGIS plugin
- Dieses Plugin erzeugt Ganglininen von Kanalnetzen
+ Plugin für einen animierten Laengsschnitt
                              -------------------
-        begin                : 2017-02-09
-        copyright            : (C) 2017 by Michael Gesenhues
-        email                : Gesenhues@fh-aachen.de
+        begin                : 2017-02-16
+        copyright            : (C) 2017 by Leon Ochsenfeld
+        email                : Ochsenfeld@fh-aachen.de
         git sha              : $Format:%H$
  ***************************************************************************/
 
@@ -22,14 +22,35 @@
  This script initializes the plugin, making it known to QGIS.
 """
 
+import logging, site, os.path
+from datetime import datetime as dt
+
+# Aufsetzen des Logging-Systems
+logger = logging.getLogger('QKan_Laengsschnitt')
+formatter = logging.Formatter('%(asctime)s %(name)s-%(levelname)s: %(message)s')
+ch = logging.StreamHandler()
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+dnam = dt.today().strftime("%Y%m%d")
+fnam = os.path.join(site.getuserbase(),'QKan_Laengsschnitt{}.log'.format(dnam))
+fh = logging.FileHandler(fnam)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+
+# Warnlevel setzten
+logger.setLevel(logging.DEBUG)
+ch.setLevel(logging.ERROR)
+fh.setLevel(logging.DEBUG)
+
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
-    """Load Gangliniengrafik class from file Gangliniengrafik.
+    """Load Laengsschnitt class from file Laengsschnitt.
 
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
     #
-    from .mod import Gangliniengrafik
-    return Gangliniengrafik(iface)
+    from .application import Laengsschnitt
+    return Laengsschnitt(iface)
