6a2c751f43d1fdd8b3afabecc6c75b53e57d965e
Leon Ochsenfeld <ochsenfeld@fh-aachen.de>
2017-05-31 15:20:53 +0200

    Navigations-Upgrade
    * Navigation erfolgt deutlich komplexer
    * Weniger Punkte pro Kreuzung erforderlich
    * Alle Fließrichtungen Berücksichtigt
    TODO : Logging + Dokumentation
    Fixes #8

--- navigation.py
+++ navigation.py
@@ -23,6 +23,28 @@ class Navigator:
 
     def calculate_route_haltungen(self, startpoint, endpoint=None, additional_points=None):
         """
+        * Führt die Berechnungen für die Route für jeweils den Start- und Endpunkt durch
+        * Fließrichtung ist nicht immer eindeutig
+
+        :param startpoint: Entspricht dem Haltungs-Namen aus QGis.
+        :type startpoint:str
+        :param endpoint: Entspricht dem Haltungs-Namen aus QGis.
+        :type endpoint: str
+        :param additional_points: Entspricht einer Liste von Haltungs-Namen, die zusätzlich ausgewählt wurden.
+        :type additional_points: list
+        :return:* Ob das berechnen funktioniert hat
+                * Die Route als Dictionary unterteilt in Haltungen und Schächte
+                * Ob das Plugin neu laden muss. Wenn bspw. eine falsche Datenbank übergeben wurde.
+        :rtype: (bool,dict,bool)
+        """
+        res, a, b = self.__calculate_route_haltungen(startpoint, endpoint, additional_points)
+        if res:
+            return res, a, b
+        else:
+            return self.__calculate_route_haltungen(endpoint, startpoint, additional_points)
+
+    def __calculate_route_haltungen(self, startpoint, endpoint=None, additional_points=None):
+        """
         Berechnet die Schächte und Haltungen, die zwischen einem Start- und Endpunkt liegen.
         * Start- und Endhöhe müssen nicht in der richtigen Reihenfolge übergeben werden.
         * Zusätzliche Punkte müssen nur übergeben werden, wenn der Endpunkt über mehrere Wege zu erreichen ist.
@@ -38,54 +60,14 @@ class Navigator:
                 * Ob das Plugin neu laden muss. Wenn bspw. eine falsche Datenbank übergeben wurde.
         :rtype: (bool,dict,bool)
         """
-        statement = u"""
-                SELECT sohleunten FROM (SELECT
-                         haltnam                            AS name,
-                         schunten,
-                         COALESCE(sohleunten, SU.sohlhoehe) AS sohleunten
-                       FROM haltungen
-                         LEFT JOIN
-                         (SELECT
-                            sohlhoehe,
-                            schnam
-                          FROM schaechte) AS SU ON haltungen.schunten = SU.schnam
-                       UNION
-                       SELECT
-                         wnam         AS name,
-                         schunten,
-                         SU.sohlhoehe AS sohleunten
-                       FROM wehre
-                         LEFT JOIN
-                         (SELECT
-                            sohlhoehe,
-                            schnam
-                          FROM schaechte) AS SU ON wehre.schunten = SU.schnam
-                       UNION
-                       SELECT
-                         pnam         AS name,
-                         schunten,
-                         SU.sohlhoehe AS sohleunten
-                       FROM pumpen
-                         LEFT JOIN
-                         (SELECT
-                            sohlhoehe,
-                            schnam
-                          FROM schaechte) AS SU ON pumpen.schunten = SU.schnam
-                      )
-                WHERE name="{}"
-                """
-        self.__db.sql(statement.format(startpoint))
-        start_hoehe = self.__db.fetchone()
-        self.__db.sql(statement.format(endpoint))
-        end_hoehe = self.__db.fetchone()
-        if start_hoehe < end_hoehe:
-            tmp = startpoint
+        if startpoint is None:
             startpoint = endpoint
-            endpoint = tmp
-        if additional_points is None:
-            additional_points = [endpoint]
-        else:
-            additional_points.append(endpoint)
+            endpoint = None
+        if endpoint is not None:
+            if additional_points is None:
+                additional_points = [endpoint]
+            else:
+                additional_points.append(endpoint)
         haltungen = []
         schaechte = []
         next_haltung = startpoint
@@ -127,21 +109,19 @@ class Navigator:
                 if endpoint in _next_haltungen:
                     break
                 if len(_next_haltungen) > 1:
-                    if additional_points is None:
-                        return False, "Zu viele Möglichkeiten. Bitte wählen Sie einen Wegpunkt auf dem" \
-                                      " kritischen Pfad!", False
                     solution = self.__decide_direction(_next_haltungen, additional_points)
                     if solution:
                         next_haltung = solution
                     else:
-                        return False, "Zu viele Möglichkeiten oder Pfad unerreichbar. Bitte wählen Sie einen Wegpunkt" \
-                                      " auf dem kritischen Pfad!", False
+                        return False, u"Zu viele Möglichkeiten oder Pfad unerreichbar. Bitte wählen Sie einen Wegpunkt" \
+                                      u" auf dem kritischen Pfad!", False
+
                 elif len(_next_haltungen) == 0:
-                    return False, "Endpunkt nicht erreichbar!", False
+                    return False, u"Endpunkt nicht erreichbar!", False
                 else:
                     next_haltung = _next_haltungen[0]
             except TypeError as e:
-                return False, "Endpunkt nicht erreichbar!", False
+                return False, u"Endpunkt nicht erreichbar!", False
         haltungen.append(endpoint)
         statement = u"""
         SELECT schoben
@@ -186,22 +166,7 @@ WHERE name = "{}"
         route["haltunginfo"] = haltung_info
         return True, route, False
 
-    def __decide_direction(self, options, additional_points):
-        """
-        Falls die Navigation über Kreuzungen führt, wird diese Funktion ausgeführt.
-        * Entscheidet welchen Zweig die Navigation wählt.
-        * Berechnet für alle möglichen Optionen die Pfade und guckt, ob eine der Haltungen innerhalb der 
-        zusätzlichen Punkte liegt.
-        * Wird für jede weitere Entscheidung rekursiv ausgeführt.
-
-        :param options: Entspricht einer Liste der Haltungs-Namen, die an der Kreuzung liegen.
-        :type options: list
-        :param additional_points: Entspricht einer Liste der Haltungs-Namen, die zur Entscheidung führen sollen.
-        :type additional_points: list
-        :return:* Gibt den nächsten Haltungs-Namen zurück, wenn diese auf dem richtigen Pfad liegt.
-                * Gibt False zurück, wenn der Endpunkt nicht erreichbar ist.
-        :rtype: str
-        """
+    def __get_possibilities(self, options, additional_points):
         statement = u"""
         SELECT name
         FROM (SELECT
@@ -232,38 +197,36 @@ WHERE name = "{}"
                            FROM pumpen)
                WHERE name = "{}")
                 """
-        for x in options:
-            if x in additional_points:
-                return x
-            self.__db.sql(statement.format(x))
-            _next_haltungen = self.__db.fetchall()
-            if len(_next_haltungen) > 1:
-                solution = self.__decide_direction(_next_haltungen, additional_points)
-                if solution:
-                    next_haltung = solution
-                else:
-                    break
-            elif len(_next_haltungen) == 0:
-                break
-            else:
-                next_haltung, = _next_haltungen[0]
-            # next_haltung = None
-            while next_haltung not in additional_points:
-                self.__db.sql(statement.format(next_haltung))
-                _next_haltungen = self.__db.fetchall()
-                if len(_next_haltungen) > 1:
-                    solution = self.__decide_direction(_next_haltungen, additional_points)
-                    if solution:
-                        next_haltung = solution
-                    else:
+        possibilities = []
+        for option in options:
+            run = [option]
+            if option in additional_points:
+                possibilities.append(run)
+                continue
+            self.__db.sql(statement.format(option))
+            next_haltungen = [t[0] for t in self.__db.fetchall()]
+            if len(next_haltungen) == 0:
+                continue
+            elif len(next_haltungen) == 1:
+                run.append(next_haltungen[0])
+                self.__db.sql(statement.format(next_haltungen[0]))
+                next_haltungen = [t[0] for t in self.__db.fetchall()]
+                while len(next_haltungen) == 1:
+                    run.append(next_haltungen[0])
+                    if next_haltungen[0] in additional_points:
+                        possibilities.append(run)
                         break
-                elif len(_next_haltungen) == 0:
-                    break
-                else:
-                    next_haltung, = _next_haltungen[0]
-            if next_haltung in additional_points:
-                return x
-        return False
+                    self.__db.sql(statement.format(next_haltungen[0]))
+                    next_haltungen = [t[0] for t in self.__db.fetchall()]
+                if len(next_haltungen) == 0:
+                    continue
+            if len(next_haltungen) == 1:
+                continue
+            else:
+                for r in self.__get_possibilities(next_haltungen, additional_points):
+                    if len(r) > 0:
+                        possibilities.append(run + r)
+        return possibilities
 
     def calculate_route_schaechte(self, startpoint, endpoint, additional_points=None):
         """
@@ -338,15 +301,15 @@ WHERE schoben ="{}"
         route = None
         error = ""
         _reload = False
+        nodes = list(set(nodes))
         for start_haltung in start_haltungen:
             for end_haltung in end_haltungen:
-                _res, _route, must_reload = self.calculate_route_haltungen(start_haltung, end_haltung,
-                                                                           additional_points=nodes)
+                _res, _route, must_reload = self.calculate_route_haltungen(start_haltung, end_haltung, nodes)
                 if _res:
                     possibilities += 1
                     if possibilities > 1:
-                        return False, "Zu viele Möglichkeiten. Bitte wählen Sie einen Wegpunkt auf" \
-                                      " dem kritischen Pfad!", False
+                        return False, u"Zu viele Möglichkeiten. Bitte wählen Sie einen Wegpunkt auf" \
+                                      u" dem kritischen Pfad!", False
                     route = _route
                 else:
                     if _reload is not True:
@@ -359,6 +322,35 @@ WHERE schoben ="{}"
         route["haltunginfo"] = haltung_info
         return True, route, False
 
+    def __decide_direction(self, options, additional_points):
+        possibilities = self.__get_possibilities(options, additional_points)
+        possible_runs = []
+        for run in possibilities:
+            for p in run:
+                if p in additional_points[:-1]:
+                    possible_runs.append(run)
+        if len(possible_runs) > 0:
+            pos = -1
+            first_p = ""
+            _run = None
+            for run in possible_runs:
+                for i, p in enumerate(run):
+                    if p in additional_points:
+                        if first_p == "":
+                            first_p = p
+                            pos = i
+                            _run = run
+                            break
+                        elif first_p == p:
+                            return False
+                        else:
+                            if i < pos:
+                                _run = run
+            if _run is not None:
+                return _run[0]
+
+        return False
+
     def check_route_haltungen(self, nodes):
         """
         * Wird ausgeführt, wenn mehr als ein Start- und Endpunkt übergeben wurde.
@@ -370,76 +362,25 @@ WHERE schoben ="{}"
         :return:* Ob das berechnen funktioniert hat
                 * Die Route als Dictionary unterteilt in Haltungen und Schächte
                 * Ob das Plugin neu laden muss. Wenn bspw. eine falsche Datenbank übergeben wurde.
-        :rtype: (bool,dict,bool) 
+        :rtype: (bool,dict,bool)
         """
-        statement = u"""
-        SELECT sohleunten FROM (SELECT
-                 haltnam                            AS name,
-                 schunten,
-                 COALESCE(sohleunten, SU.sohlhoehe) AS sohleunten
-               FROM haltungen
-                 LEFT JOIN
-                 (SELECT
-                    sohlhoehe,
-                    schnam
-                  FROM schaechte) AS SU ON haltungen.schunten = SU.schnam
-               UNION
-               SELECT
-                 wnam         AS name,
-                 schunten,
-                 SU.sohlhoehe AS sohleunten
-               FROM wehre
-                 LEFT JOIN
-                 (SELECT
-                    sohlhoehe,
-                    schnam
-                  FROM schaechte) AS SU ON wehre.schunten = SU.schnam
-               UNION
-               SELECT
-                 pnam        AS name,
-                 schunten,
-                 SU.sohlhoehe AS sohleunten
-               FROM pumpen
-                 LEFT JOIN
-                 (SELECT
-                    sohlhoehe,
-                    schnam
-                  FROM schaechte) AS SU ON pumpen.schunten = SU.schnam
-              )
-        WHERE name="{}"
-        """
-        self.__db.sql(statement.format(nodes[0]))
-        startpoint = nodes[0]
-        endpoint = nodes[0]
-        try:
-            min_value, = self.__db.fetchone()
-        except TypeError:
-            return False, "Falsche Datenbank übermittelt", True
-        max_value = min_value
-        for n in nodes:
-            self.__db.sql(statement.format(n))
-            value, = self.__db.fetchone()
-            if value < min_value:
-                min_value = value
-                endpoint = n
-            elif value > max_value:
-                max_value = value
-                startpoint = n
-        try:
-            nodes.remove(startpoint)
-            nodes.remove(endpoint)
-        except ValueError:
-            pass
-        if len(nodes) == 0:
-            nodes = None
-        res, route, _reload = self.calculate_route_haltungen(startpoint, endpoint, additional_points=nodes)
-        if not res:
-            return False, route, _reload
-        if nodes is not None:
-            for n in nodes:
-                if n not in route["haltungen"]:
-                    return False, "Gegebener Pfad ist fehlerhaft!", False
-        return res, route, _reload
+        route = ""
+        for node in nodes:
+            _additional = list(nodes)
+            _additional.remove(node)
+            for a in _additional:
+                additional = list(_additional)
+                additional.remove(a)
+                res, route, _reload = self.__calculate_route_haltungen(node, a, additional)
+                if res:
+                    _result = True
+                    for n in nodes:
+                        if n not in route["haltungen"]:
+                            _result = False
+                    if _result:
+                        return res, route, _reload
+        route = u"Gegebener Pfad ist fehlerhaft" if isinstance(route, dict) else route
+        return False, route, False
 
     def check_route_schaechte(self, nodes):
         """
@@ -481,7 +422,7 @@ WHERE schoben ="{}"
         if nodes is not None:
             for n in nodes:
                 if n not in route["schaechte"]:
-                    return False, "Gegebener Pfad ist fehlerhaft!", False
+                    return False, u"Gegebener Pfad ist fehlerhaft!", False
         return res, route, _reload
 
     def __fetch_data(self, route):
@@ -597,7 +538,7 @@ WHERE schoben ="{}"
     @staticmethod
     def get_schaechte(schaechte):
         """
-        Erstellt ein Route-Objekt, welches einem Dictionary über alle Haltungen und Schächten getrennt voneinander 
+        Erswtellt ein Route-Objekt, welches einem Dictionary über alle Haltungen und Schächten getrennt voneinander
         entspricht.
 
         :param schaechte: Entspricht den selektierten Schacht-Namen aus QGis.
