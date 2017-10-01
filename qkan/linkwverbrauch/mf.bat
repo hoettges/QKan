b325a682fd565430587f18a661eda22b78d0c68e
Jörg Höttges <hoettges@fh-aachen.de>
2017-10-01 13:54:59 +0200

    Verzeichnis linkwverbrauch gelöscht
    Die Funktion zur Verknüpfung der Wasserverbräuche wurde nicht in einem eigenen Modul realisiert, sondern in LinkFl integriert. Somit entfällt das Verzeichnis

--- qkan/linkwverbrauch/mf.bat
+++
@@ -1,2 +0,0 @@
-"C:\Program Files\QGIS 2.18\bin\pyrcc4" -o resources_createlines.py resources_createlines.qrc
-"C:\Program Files\QGIS 2.18\bin\pyrcc4" -o resources_assigntezg.py resources_assigntezg.qrc
