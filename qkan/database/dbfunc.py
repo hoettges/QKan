172fe629d0cc4e944e85bb8d5720b85c6abc7350
Jörg Höttges <hoettges@fh-aachen.de>
2017-12-04 11:20:09 +0100

    Version 2.2.7
    Anpassung Formulare

--- qkan/database/dbfunc.py
+++ qkan/database/dbfunc.py
@@ -187,7 +187,7 @@ class DBConnection:
 
     # Versionskontrolle der QKan-Datenbank
 
-    def version(self, actversion = u'2.2.6'):
+    def version(self, actversion = u'2.2.7'):
         """Checks database version. Database is just connected by the calling procedure.
 
             :param actversion: aktuelle Version
