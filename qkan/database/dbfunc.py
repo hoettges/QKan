4f9288a7979b16aa6dcc1dae000d2a6fd0db0712
Jörg Höttges <hoettges@fh-aachen.de>
2018-02-11 10:56:32 +0100

    Version 2.18
    Fehler im Export bei Gebietsauswahl beseitigt

--- qkan/database/dbfunc.py
+++ qkan/database/dbfunc.py
@@ -78,7 +78,7 @@ class DBConnection:
         self.sqltime = self.sqltime.now()
         self.sqltext = ''
         self.sqlcount = 0
-        self.actversion = '2.2.17'
+        self.actversion = '2.2.18'
         self.templatepath = os.path.join(pluginDirectory('qkan'), u"database/templates")
 
         if dbname is not None:
