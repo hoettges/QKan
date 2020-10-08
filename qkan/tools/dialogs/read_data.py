b7a616c25ba010ed7fa65a30da507e848410e183
Yannick Linke <invisi@0x0f.net>
2020-10-08 13:56:23 +0200

    read_data dialog: Import pasted data into temporary database

--- qkan/tools/dialogs/read_data.py
+++ qkan/tools/dialogs/read_data.py
@@ -1,16 +1,18 @@
 import logging
 import os
 import sqlite3
-from pathlib import Path
-from sqlite3 import Connection, Cursor
-
+import time
 import typing
+from pathlib import Path
+from sqlite3 import Connection
+
 from qgis.PyQt import uic
 from qgis.PyQt.QtWidgets import (
     QComboBox,
     QDialogButtonBox,
     QPushButton,
 )
+from qgis.core import QgsApplication
 from qgis.utils import spatialite_connect
 
 from qkan import QKan
@@ -33,6 +35,19 @@ REQUIRED_FIELDS = {
     "pumpen": ["pnam"],
     "wehre": ["wnam"],
 }
+IMPORT_TYPES = {
+    "schnam": "TEXT",
+    "xsch": "REAL",
+    "ysch": "REAL",
+    "sohlhoehe": "REAL",
+    "xschob": "REAL",
+    "yschob": "REAL",
+    "xschun": "REAL",
+    "yschun": "REAL",
+    "pnam": "TEXT",
+    "wnam": "TEXT",
+}
+CF_TEXT = 1
 
 
 class ReadDataDialog(QKanDBDialog, FORM_CLASS_read_data):
@@ -58,8 +73,82 @@ class ReadDataDialog(QKanDBDialog, FORM_CLASS_read_data):
         self.tf_qkanDB.textChanged.connect(self.reload_database)
 
         self.other_db: typing.Optional[Connection] = None
+        self.temporary_db: typing.Optional[Connection] = None
         self.db_name: typing.Optional[str] = None
-        self.cursor: typing.Optional[Cursor] = None
+
+        self.pbPasteAuslaesse.clicked.connect(lambda: self.read_clipboard("auslaesse"))
+        self.pbPasteHaltungen.clicked.connect(lambda: self.read_clipboard("haltungen"))
+        self.pbPastePumpen.clicked.connect(lambda: self.read_clipboard("pumpen"))
+        self.pbPasteSchaechte.clicked.connect(lambda: self.read_clipboard("schaechte"))
+        self.pbPasteSpeicher.clicked.connect(lambda: self.read_clipboard("speicher"))
+        self.pbPasteWehre.clicked.connect(lambda: self.read_clipboard("wehre"))
+
+    def find_compatible_tables(self):
+        cursors = []
+
+        if self.other_db:
+            cursors.append(self.other_db.cursor())
+        if self.temporary_db:
+            cursors.append(self.temporary_db.cursor())
+
+        compatible: typing.Dict[str, typing.List[str]] = {
+            _: [] for _ in REQUIRED_FIELDS.keys()
+        }
+
+        for cursor in cursors:
+            # Get all tables
+            cursor.execute(
+                """
+                SELECT name FROM sqlite_master 
+                WHERE type="table" AND name NOT LIKE "sqlite_%";
+                """
+            )
+
+            data = cursor.fetchall()
+            table_names = [_[0] for _ in data]
+
+            # Get all columns
+            table_columns = {}
+            for name in table_names:
+                cursor.execute("SELECT name FROM PRAGMA_TABLE_INFO(?);", (name,))
+                data = cursor.fetchall()
+                table_columns[name] = [_[0] for _ in data]
+
+            # Filter available tables
+            for name, fields in table_columns.items():
+                for table_name, table_fields in REQUIRED_FIELDS.items():
+                    if (
+                        name.startswith("idx_")
+                        or name.startswith("views_")
+                        or name.startswith("virts_")
+                    ):
+                        continue
+                    # Skip table
+                    if any(required not in fields for required in table_fields):
+                        # logger.debug("Skipping %s for %s", name, table_name)
+                        continue
+
+                    compatible.get(table_name, []).append(name)
+
+        # Fill dropdowns
+        for dropdown, layers in compatible.items():
+            target = self.get_target(dropdown)
+            # TODO: Skip dropdowns if they shouldn't be updated
+
+            # Reset dropdown
+            target.clear()
+            target.addItem("--")
+
+            for idx, layer in enumerate(layers):
+                target.addItem(layer)
+                # TODO: Add tooltip
+                # target.setItemData(idx, layer.source(), Qt.ToolTipRole)
+
+        for cursor in cursors:
+            try:
+                cursor.close()
+            except sqlite3.DatabaseError:
+                pass
 
     def reload_database(self):
         db_name = self.tf_qkanDB.text()
@@ -69,61 +158,21 @@ class ReadDataDialog(QKanDBDialog, FORM_CLASS_read_data):
         logger.info("Reloading database, switching to %s", db_name)
 
         if self.other_db:
-            self.cursor.close()
             self.other_db.close()
 
         try:
             self.other_db = spatialite_connect(
                 database=db_name, check_same_thread=False
             )
-            self.cursor = self.other_db.cursor()
             self.db_name = db_name
         except sqlite3.DatabaseError:
             # TODO: Show tooltip and mark field red
             logger.exception("Failed to open database")
             return
 
-        compatible: typing.Dict[str, typing.List[str]] = {
-            _: [] for _ in REQUIRED_FIELDS.keys()
-        }
+        self.find_compatible_tables()
 
-        # Get all tables
-        self.cursor.execute(
-            """
-            SELECT name FROM sqlite_master 
-            WHERE type="table" AND name NOT LIKE "sqlite_%";
-            """
-        )
-
-        data = self.cursor.fetchall()
-        table_names = [_[0] for _ in data]
-
-        # Get all columns
-        table_columns = {}
-        for name in table_names:
-            self.cursor.execute(f'SELECT name FROM PRAGMA_TABLE_INFO("{name}");')
-            data = self.cursor.fetchall()
-            table_columns[name] = [_[0] for _ in data]
-
-        # Filter available tables
-        for name, fields in table_columns.items():
-            for table_name, table_fields in REQUIRED_FIELDS.items():
-                # Skip table
-                if any(required not in fields for required in table_fields):
-                    continue
-
-                compatible.get(table_name, []).append(name)
-
-        # Fill dropdowns
-        for dropdown, layers in compatible.items():
-            target = self.get_target(dropdown)
-            target.clear()
-            for idx, layer in enumerate(layers):
-                target.addItem(layer)
-                # TODO: Add tooltip
-                # target.setItemData(idx, layer.source(), Qt.ToolTipRole)
-
-    def get_target(self, name: str):
+    def get_target(self, name: str) -> QComboBox:
         return {
             "auslaesse": self.cbLayerAuslaesse,
             "schaechte": self.cbLayerSchaechte,
@@ -136,9 +185,6 @@ class ReadDataDialog(QKanDBDialog, FORM_CLASS_read_data):
     def run(self):
         """
         TODO: Checkbox to list all tables in dropdowns
-        TODO: Paste -> Insert table into temporary table in current database
-              or temporary DB
-             `CREATE TEMPORARY TABLE temp_table1( name TEXT );`
         """
         self.tf_qkanDB.setText(QKan.config.database.qkan)
 
@@ -146,3 +192,57 @@ class ReadDataDialog(QKanDBDialog, FORM_CLASS_read_data):
         if self.exec_():
             # TODO: Write the actual import queries
             print("Import here")
+
+    def read_clipboard(self, table_suffix: str):
+        data = QgsApplication.clipboard().text()
+        parsed_data = []
+        if data:
+            lines = data.splitlines()
+            if len(lines) == 1:
+                return
+
+            headers = lines[0].split("\t")
+            for header in headers:
+                if header not in IMPORT_TYPES:
+                    continue
+
+            for row, line in enumerate(lines[1:]):
+                parsed_data.append([])
+                x = line.split("\t")
+                for column, value in enumerate(x):
+                    _type = IMPORT_TYPES.get(headers[column], None)
+                    if not _type:
+                        continue
+
+                    if _type == "REAL":
+                        parsed_data[row].append(float(value))
+                    elif _type == "TEXT":
+                        parsed_data[row].append(value)
+
+            create_types = [
+                f"{header} {IMPORT_TYPES.get(header, 'TEXT')}" for header in headers
+            ]
+
+            table_name = f"temp{int(time.time()) % 10000}_{table_suffix}"
+
+            sql = f"CREATE TABLE {table_name} ({', '.join(create_types)});"
+
+            if not self.temporary_db:
+                self.temporary_db = spatialite_connect(
+                    database=":memory:", check_same_thread=False
+                )
+
+            # Insert table
+            cursor = self.temporary_db.cursor()
+            cursor.execute(sql)
+
+            # Insert table data
+            for row in parsed_data:
+                cursor.execute(
+                    f"INSERT INTO {table_name} VALUES ({', '.join(['?'] * len(create_types))})",
+                    row,
+                )
+
+            cursor.close()
+
+            self.find_compatible_tables()
