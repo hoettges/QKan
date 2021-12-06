e9ccccb4aa5f2226b7599fc58a7f98e6203c5663
Joerg Hoettges <hoettges@fh-aachen.de>
2021-12-06 09:41:11 +0100

    Minor changes: ks-Value, Kreuzende Haltungen
    Faktor 1000 für ks-Wert entfernt, Plausibilitätsabfrage "Kreuzende Haltungen" überarbeitet

--- qkan/templates/Plausibilitaetspruefungen.sql
+++ qkan/templates/Plausibilitaetspruefungen.sql
@@ -12,82 +12,99 @@ SELECT pn.gruppe, pn.warntext, pn.warntyp, pn.warnlevel, pn.sql, pn.layername, p
 ('HYSTEM-EXTRAN', 'Schwerpunktlaufzeiten fehlen', 'Fehler', 9, 'SELECT flnam, printf("Spalte schwerpunktlaufzeit in %d Datensätzen leer (nur 5 Datensätze exemplarisch aufgelistet)", (SELECT count(*) FROM tezg WHERE schwerpunktlaufzeit IS NULL)) AS bemerkung FROM tezg WHERE schwerpunktlaufzeit IS NULL LIMIT 5', 'Haltungsflächen', 'flnam'),
 ('HYSTEM-EXTRAN', 'Simulationsstatus fehlt oder nicht in Tabelle "Simulationsstatus"', 'Fehler', 9, 'SELECT s.schnam, printf("Spalte simstatus = %s in Spalte leer oder nicht in Simulationsstatus (nur 1 exemplarisch aufgelistet!)", s.simstatus) AS bemerkung FROM schaechte AS s LEFT JOIN simulationsstatus AS u ON s.simstatus = u.bezeichnung WHERE u.bezeichnung IS NULL GROUP BY s.simstatus', 'Schächte', 'schnam'),
 ('Kreuzende Haltungen', 'Kreuzende Haltungen', 'Warnung', 6, 
-'SELECT
-     haltna1 AS haltnam, 
-     printf("Theoretischer Abstand zu Haltung %s beträgt d = %.2f", haltna2, abs((huax-hoax)*vx+(huay-hoay)*vy+(huaz-hoaz)*vz)/SQRT(vx*vx+vy*vy+vz*vz)) as bemerkung
+'SELECT 
+  haltna1, haltna2, hoehob, hoehun, 
+  abs((p3x-p1x)*ex+(p3y-p1y)*ey+(p3z-p1z)*ez)/SQRT(ex*ex+ey*ey+ez*ez) AS abstkreuz, 
+  abs(L13/SQRT(L12))         AS abstpar,
+  ((sx-p1x)*(p2x-p1x)+(sy-p1y)*(p2y-p1y))*((sx-p2x)*(p2x-p1x)+(sy-p2y)*(p2y-p1y)) AS d1, 
+  ((sx-p3x)*(p4x-p3x)+(sy-p3y)*(p4y-p3y))*((sx-p4x)*(p4x-p3x)+(sy-p4y)*(p4y-p3y)) AS d2, 
+  MAX(0, L12, L13, L14)-MIN(0, L12, L13, L14)-ABS(L12)-ABS(L34)                   AS d3
 FROM (
   SELECT 
-    haltna1, haltna2, durchob, durchun, hoehob, hoehun, 
-    hoax, hoay, hoaz, hoex, hoey, hoez, huax, huay, huaz, huex, huey, huez, 
-    (hoey-hoay)*(huez-huaz)-(hoez-hoaz)*(huey-huay) AS vx, 
-    (hoez-hoaz)*(huex-huax)-(hoex-hoax)*(huez-huaz) AS vy,
-    (hoex-hoax)*(huey-huay)-(hoey-hoay)*(huex-huax) AS vz
+    haltna1, haltna2, hoehob, hoehun, 
+    p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z, p4x, p4y, p4z, ex, ey, ez,
+    CASE WHEN abs(det) > 0.000001 * bet THEN
+      p1x+(p2x-p1x)*((p3x-p1x)*(p3y-p4y)-(p3y-p1y)*(p3x-p4x))/det ELSE NULL END     AS sx,
+    CASE WHEN abs(det) > 0.000001 * bet THEN
+      p1y+(p2y-p1y)*((p3x-p1x)*(p3y-p4y)-(p3y-p1y)*(p3x-p4x))/det ELSE NULL END     AS sy,
+    ((p2x-p1x)*(p2x-p1x)+(p2y-p1y)*(p2y-p1y))                                       AS L12,
+    ((p3x-p1x)*(p2x-p1x)+(p3y-p1y)*(p2y-p1y))                                       AS L13,
+    ((p4x-p1x)*(p2x-p1x)+(p4y-p1y)*(p2y-p1y))                                       AS L14,
+    ((p4x-p3x)*(p2x-p1x)+(p4y-p3y)*(p2y-p1y))                                       AS L34
   FROM (
-    SELECT
-      ho.haltnam            AS haltna1, 
-      hu.haltnam            AS haltna2, 
-      ho.durchm             AS durchob, 
-      hu.durchm             AS durchun, 
-      ho.hoehe              AS hoehob, 
-      hu.hoehe              AS hoehun, 
-      x(PointN(ho.geom,1))  AS hoax, 
-      y(PointN(ho.geom,1))  AS hoay, 
-      ho.sohleoben          AS hoaz, 
-      x(PointN(ho.geom,-1)) AS hoex, 
-      y(PointN(ho.geom,-1)) AS hoey, 
-      ho.sohleunten         AS hoez, 
-      x(PointN(hu.geom,1))  AS huax, 
-      y(PointN(hu.geom,1))  AS huay, 
-      hu.sohleoben          AS huaz, 
-      x(PointN(hu.geom,-1)) AS huex, 
-      y(PointN(hu.geom,-1)) AS huey, 
-      hu.sohleunten         AS huez
+    SELECT 
+      haltna1, haltna2, hoehob, hoehun, 
+      p1x, p1y, p1z, p2x, p2y, p2z, p3x, p3y, p3z, p4x, p4y, p4z, 
+      (p2y-p1y)*(p4z-p3z)-(p2z-p1z)*(p4y-p3y) AS ex, 
+      (p2z-p1z)*(p4x-p3x)-(p2x-p1x)*(p4z-p3z) AS ey,
+      (p2x-p1x)*(p4y-p3y)-(p2y-p1y)*(p4x-p3x) AS ez,
+      ((p2x-p1x)*(p3y-p4y)-(p2y-p1y)*(p3x-p4x)) AS det,
+      ((p2x-p1x)*(p3x-p4x)+(p2y-p1y)*(p3y-p4y)) AS bet
     FROM (
       SELECT
-        ha.haltnam AS haltnam, 
-        ha.schoben AS schoben,
-        ha.schunten AS schunten,
-        ha.geom AS geom, 
-        COALESCE(ha.sohleoben, so.sohlhoehe) AS sohleoben, 
-        COALESCE(ha.sohleunten, su.sohlhoehe) AS sohleunten,
-        MAX(ha.hoehe, ha.breite) AS durchm, 
-        CASE WHEN ha.hoehe = 0 THEN ha.breite ELSE ha.hoehe END AS hoehe
-      FROM haltungen AS ha 
-      INNER JOIN schaechte AS so 
-      ON ha.schoben = so.schnam
-      INNER JOIN schaechte AS su 
-      ON ha.schunten = su.schnam
-    ) AS ho 
-    INNER JOIN (
-      SELECT
-        ha.haltnam AS haltnam, 
-        ha.schoben AS schoben,
-        ha.schunten AS schunten,
-        ha.geom AS geom, 
-        COALESCE(ha.sohleoben, so.sohlhoehe) AS sohleoben, 
-        COALESCE(ha.sohleunten, su.sohlhoehe) AS sohleunten,
-        MAX(ha.hoehe, ha.breite) AS durchm, 
-        CASE WHEN ha.hoehe = 0 THEN ha.breite ELSE ha.hoehe END AS hoehe
-      FROM haltungen AS ha 
-      INNER JOIN schaechte AS so 
-      ON ha.schoben = so.schnam
-      INNER JOIN schaechte AS su 
-      ON ha.schunten = su.schnam
-    ) AS hu
-    ON Distance(ho.geom, hu.geom) < 0.5 + (ho.durchm + hu.durchm) / 2.0
-    WHERE
-      ho.haltnam <> hu.haltnam AND 
-      ho.schoben not in (hu.schoben, hu.schunten) AND 
-      ho.schunten not in (hu.schoben, hu.schunten) AND 
-      ho.sohleoben + ho.sohleunten >= hu.sohleoben + hu.sohleunten
+        ho.haltnam            AS haltna1, 
+        hu.haltnam            AS haltna2, 
+        ho.hoehe              AS hoehob, 
+        hu.hoehe              AS hoehun, 
+        x(PointN(ho.geom,1))  AS p1x, 
+        y(PointN(ho.geom,1))  AS p1y, 
+        ho.zoben              AS p1z, 
+        x(PointN(ho.geom,-1)) AS p2x, 
+        y(PointN(ho.geom,-1)) AS p2y, 
+        ho.zunten             AS p2z, 
+        x(PointN(hu.geom,1))  AS p3x, 
+        y(PointN(hu.geom,1))  AS p3y, 
+        hu.zoben              AS p3z, 
+        x(PointN(hu.geom,-1)) AS p4x, 
+        y(PointN(hu.geom,-1)) AS p4y, 
+        hu.zunten             AS p4z
+      FROM (
+        SELECT
+          ha.haltnam AS haltnam, 
+          ha.schoben AS schoben,
+          ha.schunten AS schunten,
+          ha.geom AS geom, 
+          COALESCE(ha.sohleoben, so.sohlhoehe)  + COALESCE(ha.hoehe, ha.breite)*0.5 AS zoben, 
+          COALESCE(ha.sohleunten, su.sohlhoehe) + COALESCE(ha.hoehe, ha.breite)*0.5 AS zunten,
+          COALESCE(ha.hoehe, ha.breite) AS durchm, 
+          CASE WHEN COALESCE(ha.hoehe, 0) = 0 THEN ha.breite ELSE ha.hoehe END AS hoehe
+        FROM haltungen AS ha 
+        INNER JOIN schaechte AS so 
+        ON ha.schoben = so.schnam
+        INNER JOIN schaechte AS su 
+        ON ha.schunten = su.schnam
+      ) AS ho 
+      INNER JOIN (
+        SELECT
+          ha.haltnam AS haltnam, 
+          ha.schoben AS schoben,
+          ha.schunten AS schunten,
+          ha.geom AS geom, 
+          COALESCE(ha.sohleoben, so.sohlhoehe)  + COALESCE(ha.hoehe, ha.breite)*0.5 AS zoben, 
+          COALESCE(ha.sohleunten, su.sohlhoehe) + COALESCE(ha.hoehe, ha.breite)*0.5 AS zunten,
+          COALESCE(ha.hoehe, ha.breite) AS durchm, 
+          CASE WHEN COALESCE(ha.hoehe, 0) = 0 THEN ha.breite ELSE ha.hoehe END AS hoehe
+        FROM haltungen AS ha 
+        INNER JOIN schaechte AS so 
+        ON ha.schoben = so.schnam
+        INNER JOIN schaechte AS su 
+        ON ha.schunten = su.schnam
+      ) AS hu
+      ON Distance(ho.geom, hu.geom) < 0.5 + (ho.durchm + hu.durchm) / 2.0
+      WHERE
+        ho.haltnam <> hu.haltnam AND 
+        ho.schoben not in (hu.schoben, hu.schunten) AND 
+        ho.schunten not in (hu.schoben, hu.schunten) AND 
+        ho.zoben + ho.zunten >= hu.zoben + hu.zunten
+      )
     )
   )
 WHERE
-  abs((huax-hoax)*vx+(huay-hoay)*vy+(huaz-hoaz)*vz)/SQRT(vx*vx+vy*vy+vz*vz) <= 0.5 +
-  CASE WHEN MAX(huaz, huez) + durchun < MIN(hoaz, hoez) 
-    THEN hoehun
-    ELSE (durchob + durchun) / 2.0 
-  END
+CASE WHEN d1 IS NOT NULL AND d1 <= 0 AND d2 <= 0
+    THEN abstkreuz <= (hoehob + hoehun) / 2.0 + 0.5
+WHEN d1 IS NULL AND d3 <= 0
+    THEN abstpar <= (hoehob + hoehun) / 2.0 + 0.5
+ELSE FALSE END
 ', 'Haltungen nach Typ', 'haltnam'))) AS pn
 LEFT JOIN pruefsql AS ps
 ON (pn.gruppe = ps.gruppe AND pn.warntext = ps.warntext)
