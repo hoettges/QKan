90076173985576389c2bc85741dceb75f3fced4a
Benedykt Pasek <benedykt.pasek@erftverband.de>
2026-09-24 13:24:59 +0200

    QKan-Version an qkan3 angepasst

--- qkan/__init__.py
+++ qkan/__init__.py
@@ -167,8 +167,8 @@ class QKan:
     template_dir: str
     forms: list[str]
 
-    dbVersion = "3.4.12"  # Version der QKan-Datenbank
-    qgsVersion = "3.4.19"  # Version des Projektes und der Projektdatei. Kann höher als die der QKan-Datenbank sein
+    dbVersion = "3.4.20"  # Version der QKan-Datenbank
+    qgsVersion = "3.4.20"  # Version des Projektes und der Projektdatei. Kann höher als die der QKan-Datenbank sein
     build = "0000"
 
     # SQL-Statements werden abhängig vom Datenbanktyp und Modul geladen.
